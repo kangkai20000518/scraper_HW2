5fdaf98c3ea62293cece77e06a75f0a64a4cb94c
kangkai20000518 <103068187+kangkai20000518@users.noreply.github.com>
2022-04-26 23:33:54 -0700

    renew

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -9,7 +9,7 @@ class ImdbSpider(scrapy.Spider):
     start_urls = ['https://www.imdb.com/title/tt0898266/']
 
     def parse(self,response):
-        next_page = response.css("a[href*='fullcredits']").attrib["href"]
+        next_page = response.css("a[href*='fullcredits']").attrib['href']
         if next_page: # identical to if next_page is not None
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse_full_credits)
@@ -24,7 +24,7 @@ class ImdbSpider(scrapy.Spider):
 
     def parse_actor_page(self,response):
         name=response.css(".header").css("span.itemprop::text").get()
-        for movie in response.css("div.filmo-category-section::not([style*='displsy::none;']) b"):
+        for movie in response.css("div.filmo-category-section::not([style*='display:none;']) b"):
             yield{
                 "name":name,
                 "movie_name":movie.css("a::text").get()
